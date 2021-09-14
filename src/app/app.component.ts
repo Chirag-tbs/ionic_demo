f155bc5ae1dc7162738b6a020012a9d4720d49ce
Chirag-tbs <58430999+Chirag-tbs@users.noreply.github.com>
2021-09-14 12:28:05 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor() {
+    <script type="text/javascript" charset="utf-8">
+      var port = location.search.substr("?port=".length);
+      document.write("<img src='http://127.0.0.1:" + port + "/logo.png' />");
+
+    </script>
+  }
 }
