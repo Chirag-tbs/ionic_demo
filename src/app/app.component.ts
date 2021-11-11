c9ab4280d0e9732adc8beb454f6b1b93361e5ad8
Chirag-tbs <58430999+Chirag-tbs@users.noreply.github.com>
2021-11-11 12:36:25 +0530

    change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,6 @@ export class AppComponent {
     <script type="text/javascript" charset="utf-8">
       var port = location.search.substr("?port=".length);
       document.write("<img src='http://127.0.0.1:" + port + "/logo.png' />");
-
     </script>
   }
 }
